578362f7d7e080e39091a614f3ef6b8614c2e9bd
Marduk Bolaños <marduk.bolanos@imbus.de>
2022-07-08 19:02:01 +0200

    Configured dotnet publish in preparation for distributing RoboSAPiens as a Python wheel.

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @echo off
 
-dotnet publish -c release -r win-x86 -p:PublishSingleFile=true -p:PublishTrimmed=true
-copy .\bin\Debug\netcoreapp5.0\RoboSAPiens.html .\bin\release\netcoreapp5.0\win-x86\publish
-warp-packer.exe --arch windows-x86 --input_dir .\bin\release\netcoreapp5.0\win-x86\publish --exec RoboSAPiens.exe --output RoboSAPiens.exe
-ResourceHacker.exe -open RoboSAPiens.exe -save RoboSAPiens.exe -action addskip -res RoboSAPiens.ico -mask ICONGROUP,MAIN,
+set OUTDIR=..\build
+rmdir /S /Q %OUTDIR%
+
+dotnet publish -c Release -p:PublishDir=%OUTDIR%
--- docgen.bat
+++ docgen.bat
@@ -1,3 +1,3 @@
 @echo off
 
-libdoc --name RoboSAPiens Remote::http://127.0.0.1:8270 .\bin\Debug\netcoreapp5.0\RoboSAPiens.html
+libdoc --name RoboSAPiens Remote::http://127.0.0.1:8270 RoboSAPiens.html
