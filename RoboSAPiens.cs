4dd2a8149027cc7ad13173188b30e6def1bdec9e
Marduk Bolaños <marduk.bolanos@imbus.de>
2022-08-10 18:04:47 +0200

    - Updated the documentation of the keyword "Verify form heading" - Disabled the keyword Compare Forms. It only performs a JSON diff and will be implemented in Python.

--- RoboSAPiens.cs
+++ RoboSAPiens.cs
@@ -104,39 +104,39 @@ namespace RoboSAPiens {
             };
         }
 
-        [Keyword("Dateiexport vergleichen"),
-         Doc("Der Dateiexport aus einer Maske wird zwischen den angegebenen Testservern verglichen.\n\n" +
-             "| ``Dateiexport vergleichen     Name     Testserver1     Testserver2     Verzeichnis``\n\n" +
-             "Verzeichnis: Der absolute Pfad des Verzeichnisses, wo der Dateiexport abgelegt ist.\n\n" + 
-             "*Hinweis*: Wenn Unterschiede gefunden wurden, werden sie im Unterverzeichnis \"Unterschiede\" bildlich dokumentiert.")]
-        public RobotResult compareForms(string Name, string Testserver1, string Testserver2, string Verzeichnis) {
-            Form form1;
-            Form form2;
+        // [Keyword("Dateiexport vergleichen"),
+        //  Doc("Der Dateiexport aus einer Maske wird zwischen den angegebenen Testservern verglichen.\n\n" +
+        //      "| ``Dateiexport vergleichen     Name     Testserver1     Testserver2     Verzeichnis``\n\n" +
+        //      "Verzeichnis: Der absolute Pfad des Verzeichnisses, wo der Dateiexport abgelegt ist.\n\n" + 
+        //      "*Hinweis*: Wenn Unterschiede gefunden wurden, werden sie im Unterverzeichnis \"Unterschiede\" bildlich dokumentiert.")]
+        // public RobotResult compareForms(string Name, string Testserver1, string Testserver2, string Verzeichnis) {
+        //     Form form1;
+        //     Form form2;
 
-            try {
-                form1 = new Form(Name, Testserver1, Verzeichnis);
-            } catch (Exception e) {
-                return new ExceptionError(e, $"Die Maske konnte aus der Datei {Name}_{Testserver1}.csv nicht gelesen werden.");
-            }
+        //     try {
+        //         form1 = new Form(Name, Testserver1, Verzeichnis);
+        //     } catch (Exception e) {
+        //         return new ExceptionError(e, $"Die Maske konnte aus der Datei {Name}_{Testserver1}.csv nicht gelesen werden.");
+        //     }
 
-            try {
-                form2 = new Form(Name, Testserver2, Verzeichnis);
-            } catch (Exception e) {
-                return new ExceptionError(e, $"Die Maske konnte aus der Datei {Name}_{Testserver2}.csv nicht gelesen werden");
-            }
+        //     try {
+        //         form2 = new Form(Name, Testserver2, Verzeichnis);
+        //     } catch (Exception e) {
+        //         return new ExceptionError(e, $"Die Maske konnte aus der Datei {Name}_{Testserver2}.csv nicht gelesen werden");
+        //     }
 
-            try {
-                (var differences, var matches) = form1.compareTo(form2, Verzeichnis);
+        //     try {
+        //         (var differences, var matches) = form1.compareTo(form2, Verzeichnis);
 
-                if (differences.Count != 0) {
-                    return new NonIdenticalFormsError("Unterschiede wurden festgestellt.", differences);
-                }
+        //         if (differences.Count != 0) {
+        //             return new NonIdenticalFormsError("Unterschiede wurden festgestellt.", differences);
+        //         }
 
-                return new Success($"Die Maske '{Name}' hat den gleichen Inhalt in den Testservern '{Testserver1}' und '{Testserver2}'", matches);
-            } catch (Exception e) {
-                return new ExceptionError(e, $"Die Masken aus den Dateien {Name}_{Testserver1}.csv und {Name}_{Testserver2}.csv konnten nicht verglichen werden.");
-            }
-        }
+        //         return new Success($"Die Maske '{Name}' hat den gleichen Inhalt in den Testservern '{Testserver1}' und '{Testserver2}'", matches);
+        //     } catch (Exception e) {
+        //         return new ExceptionError(e, $"Die Masken aus den Dateien {Name}_{Testserver1}.csv und {Name}_{Testserver2}.csv konnten nicht verglichen werden.");
+        //     }
+        // }
 
         [Keyword("Verbinden mit dem SAP Server"),
          Doc("Die Verbindung mit dem angegebenen SAP Server wird hergestellt.\n\n" +
@@ -401,7 +401,8 @@ namespace RoboSAPiens {
 
         [Keyword("Überschrift überprüfen"),
          Doc("Überprüfen, ob die Maske die angegebene Überschrift hat.\n\n" +
-             "| ``Überschrift überprüfen    Überschrift``")]
+             "| ``${Ergebnis}    Überschrift überprüfen    Überschrift``\n" +
+             "| ``${Ergebnis} ist entweder \"TRUE\" oder \"FALSE\"``")]
         public RobotResult verifyFormHeading(string Ueberschrift) {
             return session switch {
                 SAPSession session => session.verifyWindowTitle(Ueberschrift),
