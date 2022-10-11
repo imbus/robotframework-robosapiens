765707f5d8e2cab37ff1d185e693d89cd1d66afa
Marduk Bolaños <marduk.bolanos@imbus.de>
2022-10-11 15:19:37 +0200

    The documentation in HTML format is no longer included. It is dynamically generated by the VS Code extensions using the RF Remote API.

--- Main.cs
+++ Main.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
-using System.Reflection;
 
 namespace RoboSAPiens {
     class _ {
@@ -13,16 +9,6 @@ namespace RoboSAPiens {
 
         static void info(params string[] messages) {
             Console.WriteLine(String.Join(Environment.NewLine, messages));
-        }
-
-        static Stream loadDocumentation(string htmlFile) {
-            return Assembly.GetExecutingAssembly().GetManifestResourceStream("RoboSAPiens.RoboSAPiens.html")!;
-        }
-
-        static void showDocumentation(HttpListenerContext context, Stream rfDocumentation) {
-            context.Response.ContentType = "text/html";
-            rfDocumentation.CopyTo(context.Response.OutputStream);
-            context.Response.OutputStream.Close();
         }
 
         static void startServer(HttpListener listener, string serverAddress) {
@@ -41,35 +27,22 @@ namespace RoboSAPiens {
 
         public static void Main(string[] args) {
             const string host = "http://127.0.0.1";
-            const string docFile = "RoboSAPiens.html";
             var options = new Options(args);
             var serverAddress = $"{host}:{options.port}";
-            var rfDocumentation = loadDocumentation(docFile);
             var httpListener = new HttpListener();
             var robotRemote = new RobotRemote(options);
 
             startServer(httpListener, serverAddress);
 
-            info($"- Die Dokumentation der verfügbaren Keywords befindet sich auf {serverAddress}/doc.");
             if (options.debug) {
                 info("========== DEBUG-Modus aktiv ==========");
-            } else {
+            } 
+            else {
                 info("- Zum Debuggen RoboSAPiens.exe --debug ausführen.");
             }
 
             while (httpListener.IsListening) {
-                var context = httpListener.GetContext();
-                Uri url = context.Request.Url!;
-
-                var endpoint = String.Join("", url.Segments);
-                switch (endpoint) {
-                    case "/doc":
-                        showDocumentation(context, rfDocumentation);
-                        break;
-                    default:
-                        robotRemote.ProcessRequest(context);
-                        break;
-                };
+                robotRemote.ProcessRequest(httpListener.GetContext());
             }
         }
     }
