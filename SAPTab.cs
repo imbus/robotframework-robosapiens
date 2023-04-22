9ab8759adc01e84598d3d133169899cb6afd4f76
Marduk Bolaños <marduk.bolanos@imbus.de>
2023-04-22 17:36:18 +0200

    Store a tab's tooltip in order to use it to find a tab

--- SAPTab.cs
+++ SAPTab.cs
@@ -2,17 +2,20 @@ using sapfewse;
 
 namespace RoboSAPiens {
     public sealed class SAPTab: ILabeled, IHighlightable {
+        string tooltip;
         string id;
         string label;
         private bool focused;
 
         public SAPTab(GuiTab tab) {
+            this.tooltip = tab.Tooltip;
             this.id = tab.Id;
             this.label = tab.Text;
         }
 
         public bool isLabeled(string label) {
-            return this.label.Equals(label);
+            return this.label == label ||
+                   this.tooltip == label;
         }
 
         public void select(GuiSession session) {
