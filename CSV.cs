b0980e4270349953446979cd1ffd1fa0a165b391
Marduk Bolaños <marduk.bolanos@imbus.de>
2023-04-24 21:42:46 +0200

    Only CSV serialization is needed

---
+++ CSV.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace RoboSAPiens {
+    public sealed record FormField(string Text, string Id, int Left, int Top, int Width, int Height): IEquatable<FormField> {
+        public bool Equals(FormField? other) {
+            if (other is null)
+                return false;
+
+            return this.Id == other.Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+
+    public class CSVWriter<T> {
+        CsvConfiguration config;
+
+        public CSVWriter(string delimiter) {
+            config = new CsvConfiguration(CultureInfo.InvariantCulture) {
+                Delimiter = delimiter
+            };
+        }
+
+        public void writeRows(string filename, List<T> records) {
+            using (var writer = new StreamWriter(filename))
+            using (var csvWriter = new CsvWriter(writer, config)) {
+                csvWriter.WriteRecords(records);
+            }
+        }
+    }
+}
