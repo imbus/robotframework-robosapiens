377ff6aa83e0043e322f6ef7c50a6ecbf64dbf28
Marduk Bolaños <mardukbp@mac.com>
2023-05-22 18:11:09 +0200

    libspecgen.py: Generate a libspec in JSON format for  graphical UIs to RoboSAPiens

---
+++ python/src/libspecgen.py
@@ -0,0 +1,31 @@
+import importlib
+import json
+import sys
+from pathlib import Path
+from libgen import get_str, rec_map_values
+
+
+localized = Path("localized")
+
+if __name__ == "__main__":
+    _, *args = sys.argv
+
+    if len(args) == 0:
+        print("Usage: python libspecgen.py lang")
+        sys.exit(0)
+
+    lang = args[0]
+    lib = importlib.import_module(f"{localized}.{lang}", ".").lib
+    libspec = {
+        "name": "RoboSAPiens" + (lang != "en")*f".{lang.upper()}",
+        "keywords": {
+            kw_name: {
+                "name": get_str(kw["name"]),
+                "args": rec_map_values(kw["args"], lambda k, v: get_str(v))
+            }
+            for kw_name, kw in lib["keywords"].items()
+        }
+    }
+
+    with open(Path(f"{lang}.json"), "w", encoding="utf-8") as file:
+        file.write(json.dumps(libspec, ensure_ascii=False, indent=4))
