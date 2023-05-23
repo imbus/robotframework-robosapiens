9e7c071067739123a6221835a82d06c8f3664fdb
Marduk Bolaños <mardukbp@mac.com>
2023-05-23 16:07:32 +0200

    Added a handle to drag keyword calls

--- web-assistant/src/js/components.js
+++ web-assistant/src/js/components.js
@@ -6,30 +6,34 @@ Sortable.mount(OnSpill);
 
 function keywordCall(name) {
     const keyword = api.keywords[name];
-    var args = '<div>';
+    var args = '<div style="padding: 2px">';
     const keyword_args = Object.entries(keyword.args);
     for (const [arg_id, arg] of keyword_args) {
       args += html`
-          <input type="text" name="${arg_id}" placeholder="${arg.name}" style="width: ${100/keyword_args.length - 2}%">
+          <input type="text" name="${arg_id}" placeholder="${arg.name}" style="width: ${100/keyword_args.length - 1}%">
       `;
     }
     args += '</div>';
  
     const keywordCall_innerHTML = html`
-      <div id="${name}">
-        <button class="primary center" onclick="callKeyword('${name}')" style="font-size:1rem; dispay:inline; border: 0px; padding: 0px; margin: 0px; background-color: #fff;">▶</button>
-        <span>${keyword.name}</span>
-        <!-- <span style="font-size:1rem;">🖼</span> -->
-        ${args}
-      </div>
+      <div class="row g-1">
+        <div class="handle col-1" style="cursor: grab; width: auto; font-size:13px;">📌</div>
+        <div id="${name}" class="col">
+          <button class="primary center" onclick="callKeyword('${name}')" style="font-size:1rem; dispay:inline; border: 0px; padding: 0px; margin: 0px; background-color: #fff;">▶</button>
+          <span>${keyword.name}</span>
+          <!-- <span style="font-size:1rem;">🖼</span> -->
+          ${args}
+        </div>
+      <div>
     `;
+
     return keywordCall_innerHTML;
   }
 
 function keywordList() {
   var keywords = '';
   for (const keyword of Object.keys(api.keywords)) {
-    keywords += html`<div class="list-group-item" id="${keyword}">${api.keywords[keyword].name}</div>`;
+    keywords += html`<div class="list-group-item" id="${keyword}" style="cursor: grab">${api.keywords[keyword].name}</div>`;
   }
 
   return html`<div id="keyword-list" class="list-group">${keywords}</div>`;
@@ -67,8 +71,10 @@ new Sortable(
     },
     removeOnSpill: true,    
     animation: 150,
+    handle: ".handle",
     onAdd: function(event) {
       event.item.innerHTML = keywordCall(event.item.id)
+      event.item.style = "cursor: auto"
     },
   }
 );
