80ebae92bf238ddba2b99f491b1b4407833c8297
Marduk Bolaños <marduk.bolanos@imbus.de>
2023-10-25 13:44:09 +0200

    Version 1.2.14

--- python/src/RoboSAPiens/DE/__init__.py
+++ python/src/RoboSAPiens/DE/__init__.py
@@ -751,4 +751,4 @@ class DE(RoboSAPiensClient):
         return super()._run_keyword('GetWindowText', args, result) # type: ignore
     
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-    ROBOT_LIBRARY_VERSION = '1.2.13'
+    ROBOT_LIBRARY_VERSION = '1.2.14'
