c43b76a9c0bb78d0251f16a53650cbc588b7ee28
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-05 17:38:03 +0100

    Version 2.15.10

--- python/src/RoboSAPiens/__init__.py
+++ python/src/RoboSAPiens/__init__.py
@@ -1,7 +1,7 @@
 from robot.api.deco import keyword
 from RoboSAPiens.client import RoboSAPiensClient
 
-__version__ = "2.15.9"
+__version__ = "2.15.10"
 
 class RoboSAPiens(RoboSAPiensClient):
     """
@@ -1444,4 +1444,4 @@ class RoboSAPiens(RoboSAPiensClient):
         return super()._run_keyword('GetWindowText', args, result) # type: ignore
     
     ROBOT_LIBRARY_SCOPE = 'SUITE'
-    ROBOT_LIBRARY_VERSION = '2.15.9'
+    ROBOT_LIBRARY_VERSION = '2.15.10'
