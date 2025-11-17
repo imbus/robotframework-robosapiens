214ad0deb42506d9a4212061d4a34c8e03c3bc62
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-11-17 08:40:46 +0100

    Version 2.22.3

--- python/src/RoboSAPiens/DE/__init__.py
+++ python/src/RoboSAPiens/DE/__init__.py
@@ -1,7 +1,7 @@
 from robot.api.deco import keyword
 from RoboSAPiens.client import RoboSAPiensClient
 
-__version__ = "2.22.2"
+__version__ = "2.22.3"
 
 class DE(RoboSAPiensClient):
     """
@@ -1756,4 +1756,4 @@ class DE(RoboSAPiensClient):
         return super()._run_keyword('MaximizeWindow', args, kwargs, result) # type: ignore
     
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-    ROBOT_LIBRARY_VERSION = '2.22.2'
+    ROBOT_LIBRARY_VERSION = '2.22.3'
