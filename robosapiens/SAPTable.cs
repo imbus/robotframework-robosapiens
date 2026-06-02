57d6c58b60f263527daedbaca38d0c34ffdeca0d
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-06-02 15:03:07 +0200

    SAPTable -> findCell by LabelColumnLocator: If the table scrollbar is not at the top move it to the top

--- robosapiens/SAPTable.cs
+++ robosapiens/SAPTable.cs
@@ -199,6 +199,12 @@ namespace RoboSAPiens {
                         if (colIndex0 > columns.Count - 1) return null;
                         if (!columns[colIndex0].titles.Contains(column)) return null;
 
+                        var table = (GuiTableControl)session.FindById(getCurrentId(session));
+                        if (table.VerticalScrollbar.Position > 0)
+                        {
+                            table.VerticalScrollbar.Position = 0;
+                        }
+
                         if (cells.Count == 0) classifyCells(session);
                         var cell = (colIndexOffset > 0) switch {
                             true => cells.findCellByLabelAndColumnIndex(label, colIndex0),
