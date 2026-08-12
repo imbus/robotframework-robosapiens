d417a5fbe08b3957791be75d768c4d2f2ae357c8
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-08-12 15:35:33 +0200

    Robosapiens: Added missing getter keywords and the method callKeyword, which converts a RF-keyword call into a KeywordCall

--- RoboSAPiens.Recorder/Robosapiens.cs
+++ RoboSAPiens.Recorder/Robosapiens.cs
@@ -4,6 +4,45 @@ namespace RoboSAPiens.Recorder
     {
         string lang = language.ToUpper();
 
+        public KeywordCall? callKeyword(string returnValue, string keyword, params string[] args)
+        {
+            return keyword.ToLower() switch
+            {
+                "get window text" or "fenstertext auslesen" => GetWindowText(returnValue),
+                "get window title" or "fenstertitel auslesen" => GetWindowTitle(returnValue),
+                "read cell" or "tabellenzelle auslesen" => args switch
+                {
+                    [string row_locator, string column] 
+                        => ReadCell(new Locator(row: row_locator, col: column), returnValue),
+                    [string row_locator, string column, string tableNumber] when int.TryParse(tableNumber, out int tableNumber_) 
+                        => ReadCell(new Locator(row: row_locator, col: column), returnValue),
+                    _ => null
+                },
+                "read statusbar" or "statusleiste auslesen" => ReadStatusbar(returnValue),
+                "read text" or "text auslesen" => args switch
+                {
+                    [string hLabel] => ReadText(new Locator(hLabel), returnValue),
+                    _ => null
+                },
+                "read text field" or "textfeld auslesen" => args switch
+                {
+                    [string hLabel] => ReadTextField(new Locator(hLabel), returnValue),
+                    _ => null
+                },
+                "read tree element" or "baumelement auslesen" => args switch
+                {
+                    [string elementPath] 
+                        => ReadTreeElement(new Locator(hLabel: elementPath), returnValue),
+                    [string elementPath, string tooltip] when bool.TryParse(tooltip, out bool tooltip_) 
+                        => ReadTreeElement(new Locator(hLabel: elementPath), returnValue, tooltip: tooltip_),
+                    [string elementPath, string tooltip, string icon] when bool.TryParse(tooltip, out bool tooltip_) && bool.TryParse(icon, out bool icon_) 
+                        => ReadTreeElement(new Locator(hLabel: elementPath), returnValue, tooltip: tooltip_, icon: icon_),
+                    _ => null
+                },
+                _ => new KeywordCall(keyword, args: [.. args.Select(arg => new KeywordCallArg(name: "name", value: arg, type: KeywordCallArgType.ARG))], returnValue: returnValue)
+            };
+        }
+
         public KeywordCall libraryImport(bool x64=false)
         {
             string library = lang switch
@@ -183,6 +222,26 @@ namespace RoboSAPiens.Recorder
             return new KeywordCall(name, args);
         }
 
+        public KeywordCall GetWindowText(string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Fenstertext auslesen",
+                _ => "Get Window Text"
+            };
+            return new KeywordCall(name, args: [], returnValue: returnValue);
+        }
+
+        public KeywordCall GetWindowTitle(string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Fenstertitel auslesen",
+                _ => "Get Window Title"
+            };
+            return new KeywordCall(name, args: [], returnValue: returnValue);
+        }
+
         public KeywordCall OpenSap(string path)
         {
             string name = lang switch
@@ -261,6 +320,87 @@ namespace RoboSAPiens.Recorder
                 locator.toKeywordCallArg(lang)
             ];
             return new KeywordCall(name, args);
+        }
+
+        public KeywordCall ReadCell(Locator locator, string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Tabellenzelle auslesen",
+                _ => "Read Cell"
+            };
+            List<KeywordCallArg> args = [
+                locator.toKeywordCallArg(lang)
+            ];
+            return new KeywordCall(name, args, returnValue: returnValue);
+        }
+
+        public KeywordCall ReadStatusbar(string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Statusleiste auslesen",
+                _ => "Read Statusbar"
+            };
+            return new KeywordCall(name, args: [], returnValue: returnValue);
+        }
+
+        public KeywordCall ReadText(Locator locator, string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Text auslesen",
+                _ => "Read Text"
+            };
+            List<KeywordCallArg> args = [
+                locator.toKeywordCallArg(lang)
+            ];
+            return new KeywordCall(name, args, returnValue: returnValue);
+        }
+
+        public KeywordCall ReadTextField(Locator locator, string returnValue)
+        {
+            string name = lang switch
+            {
+                "DE" => "Textfeld auslesen",
+                _ => "Read Text Field"
+            };
+            List<KeywordCallArg> args = [
+                locator.toKeywordCallArg(lang)
+            ];
+            return new KeywordCall(name, args, returnValue: returnValue);
+        }
+
+        public KeywordCall ReadTreeElement(Locator locator, string returnValue, bool tooltip=false, bool icon=false)
+        {
+            string name = lang switch
+            {
+                "DE" => "Baumelement auslesen",
+                _ => "Read Tree Element"
+            };
+            List<KeywordCallArg> args =
+            [
+                locator.toKeywordCallArg(lang),
+                new KeywordCallArg(
+                    name: lang switch
+                    {
+                        "DE" => "Tooltip",
+                        _ => "tooltip"
+                    },
+                    value: tooltip.ToString().Capitalize(),
+                    type: "KWARG"
+                ),
+                new KeywordCallArg(
+                    name: lang switch
+                    {
+                        "DE" => "Icon",
+                        _ => "icon"
+                    },
+                    value: icon.ToString().Capitalize(),
+                    type: "KWARG"
+                )
+            ];
+            return new KeywordCall(name, args, returnValue: returnValue);
         }
 
         public KeywordCall SelectCell(Locator locator)
